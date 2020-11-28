6dfdff7b8ce5dc4cd44fa9312dda4e1fff56eaac
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-28 18:36:35 -0500

    ADD: useInitialState hook and context

---
+++ src/context/AppContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useInitialState from '../hooks/useInitialState';
+
+const AppContext = React.createContext({});
+
+const AppProvider = ({ children }) => {
+  const initialState = useInitialState();
+
+  return (
+    <AppContext.Provider value={initialState}>{children}</AppContext.Provider>
+  );
+};
+
+export default AppProvider;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,21 +8,24 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 import Layout from '../components/Layout';
+import AppProvider from '../context/AppContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppProvider>
   );
 };
 
