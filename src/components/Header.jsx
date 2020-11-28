87351942324137d9ff4565968da493261c71cd2f
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-28 18:15:23 -0500

    ADD: Connection with React Router and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <header className="Header">
-      <h1 className="Header__title">PlatziConf Merch</h1>
-      <div className="Header-checkout">checkout</div>
+      <h1 className="Header__title">
+        <Link to="/">PlatziConf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" />
+        </Link>
+      </div>
     </header>
   );
 };
