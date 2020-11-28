98b8a25ad4f5bcb10fba2e87be09f34594350b49
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-28 17:50:06 -0500

    ADD: User information form for checkout

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
   return (
-    <div>
-      <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Información de contacto:</h2>
+        </div>
+        <div className="Information-">
+          <form action="">
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="text" placeholder="Correo electrónico" name="email" />
+            <input type="text" placeholder="Dirección" name="address" />
+            <input
+              type="text"
+              placeholder="Apto, Torre, Unidad (opcional)"
+              name="address2"
+            />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="País" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Código postal" name="zip" />
+            <input type="text" placeholder="Teléfono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
