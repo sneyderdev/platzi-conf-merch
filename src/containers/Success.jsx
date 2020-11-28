1746399bec5bbb734ea902282dfbeb24269b4db7
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-28 18:03:05 -0500

    ADD: Checkout payment and confirmation

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Oscar, gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 días a tu dirección:</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
     </div>
   );
 };
