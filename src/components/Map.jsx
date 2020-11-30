ac944c464033f1ae4182b603fa32ca0c5e613165
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-29 20:27:45 -0500

    ADD: Custom hook for Google Maps integration

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const Map = ({ data }) => {
   const mapStyles = {
     height: '50vh',
     width: '100%',
   };
 
   const defaultCenter = {
-    lat: 19.4267261,
-    lng: -99.1718796,
+    lat: data.lat,
+    lng: data.lng,
   };
 
   return (
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
   const {
     state: { buyer },
   } = useContext(AppContext);
+  const location = useGoogleAddress(buyer[0].address);
 
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, gracias por tu compra`}</h2>
+        <h2>{`${buyer[0].name}, gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
